05f593b96d903bc75973d4f572610d571c524fa5
tsenturion <sony1920145@gmail.com>
2024-09-18 21:41:49 +0500

    marks

--- top.py
+++ top.py
@@ -1,9 +1,17 @@
 """https://github.com/tsenturion/learning"""
 
+from re import *
+
 try:
     with open('pupils.txt', 'r', encoding='utf-8') as file:
         for line in file:
-            print(line)
+            """print(line.split())
+            print(line[len(line) - 2])
+            for i in line:
+                if i.isdigit():
+                    print(i)"""
+            matches = search(r'\d', line)
+            print(int(matches.group()))
 except FileNotFoundError as ex:
     print(ex)
     print(FileNotFoundError)
